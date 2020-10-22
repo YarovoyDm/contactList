a1815f01f79f273f44450e390f69289081084285
Дмитрий Яровой <yarovoy.dmytro@gmail.com>
2020-10-22 15:04:59 +0300

    some changes

---
+++ build/precache-manifest.a15d91d85161ffd031cd6bdc884aaba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a898908a1861c01564c9acc6a6aecf8",
+    "url": "/contactList/build/index.html"
+  },
+  {
+    "revision": "dc51f01f8c26b3e054c7",
+    "url": "/contactList/build/static/css/2.0aa0f16f.chunk.css"
+  },
+  {
+    "revision": "bcdfa67eb1fcfb4a317e",
+    "url": "/contactList/build/static/css/main.89ce2fd4.chunk.css"
+  },
+  {
+    "revision": "dc51f01f8c26b3e054c7",
+    "url": "/contactList/build/static/js/2.1abcf2ad.chunk.js"
+  },
+  {
+    "revision": "b28a47af2e48da47b084c56123ff1325",
+    "url": "/contactList/build/static/js/2.1abcf2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcdfa67eb1fcfb4a317e",
+    "url": "/contactList/build/static/js/main.04ea4f95.chunk.js"
+  },
+  {
+    "revision": "1bc4c5ac39d2013925de",
+    "url": "/contactList/build/static/js/runtime-main.d53276b7.js"
+  }
+]);
